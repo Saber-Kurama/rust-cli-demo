f5d4f268c48d9c1e89a827f7fa1d138ab7afe12b
guochao <463561248@qq.com>
2021-12-09 18:41:12 +0800

    ✨ feat: 宏

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,66 @@
-use structopt::clap::crate_version;
-use structopt::StructOpt;
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "rust-gopher-friend-cli", version = crate_version!(), about = "Gopher CLI application written in Rust.")]
-enum Command {
-    /// This command will get the desired Gopher
-    Get {
-        /// Gopher type
-        #[structopt()]
-        gopher: String,
-    },
+macro_rules! say_hello {
+    () => {
+        println!("学习宏")
+    };
+}
+macro_rules! create_function {
+    // 此宏接受一个 `ident` 指示符表示的参数，并创建一个名为 `$func_name` 的函数。
+    // `ident` 指示符用于变量名或函数名
+    ($func_name:ident) => (
+        fn $func_name() {
+            // `stringify!` 宏把 `ident` 转换成字符串。
+            println!("You called {:?}()",
+                     stringify!($func_name))
+        }
+    )
 }
 
+// 借助上述宏来创建名为 `foo` 和 `bar` 的函数。
+create_function!(foo);
+create_function!(bar);
+
+macro_rules! print_result {
+    // 此宏接受一个 `expr` 类型的表达式，并将它作为字符串，连同其结果一起
+    // 打印出来。
+    // `expr` 指示符表示表达式。
+    ($expression:expr) => (
+        // `stringify!` 把表达式*原样*转换成一个字符串。
+        println!("{:?} = {:?}",
+                 stringify!($expression),
+                 $expression)
+    )
+}
 fn main() {
-    let cmd = Command::from_args();
-    match cmd {
-        Command::Get { gopher } => println!("{}", gopher),
-    }
+    say_hello!();
+    foo();
+    bar();
+
+    print_result!(1u32 + 1);
+
+    // 回想一下，代码块也是表达式！
+    print_result!({
+        let x = 1u32;
+
+        x * x + 2 * x - 1
+    });
 }
+// use structopt::clap::crate_version;
+// use structopt::StructOpt;
+
+// #[derive(StructOpt, Debug)]
+// #[structopt(name = "rust-gopher-friend-cli", version = crate_version!(), about = "Gopher CLI application written in Rust.")]
+// enum Command {
+//     /// This command will get the desired Gopher
+//     Get {
+//         /// Gopher type
+//         #[structopt()]
+//         gopher: String,
+//     },
+// }
+
+// fn main() {
+//     let cmd = Command::from_args();
+//     match cmd {
+//         Command::Get { gopher } => println!("{}", gopher),
+//     }
+// }
